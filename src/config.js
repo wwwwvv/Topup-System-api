a31e92aeb178387e14edb421311b9fd4db7f68a2
StratosBlur <wave_onlyone@hotmail.com>
2019-04-18 22:57:20 +0700

    Add docker support

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const config = {
 	port: 2019,
 	SECRET: 'watdoumean',
 	mongouri:
-		'mongodb+srv://dbza:waveza555@cluster0-dccrs.mongodb.net/test?retryWrites=true',
+		process.env.mongouri|| 'mongodb+srv://dbza:waveza555@cluster0-dccrs.mongodb.net/test?retryWrites=true',
 };
 
 module.exports = config;
