092b64c412c86932cbcdbd9d37961cd745d516a5
StratosBlur <wave_onlyone@hotmail.com>
2019-04-21 03:35:01 +0700

    Remove unused console.log

--- src/controller/customers.js
+++ src/controller/customers.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const CustomerModel = require('../models/Customers');
 
 function CustomerFormat(data) {
-	console.log(data);
 	return {
 		gameId: `${data.gameId}`,
 		fullname: `${data.fullname}`,
